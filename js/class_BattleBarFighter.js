c657ff251629fb0f92d2d677287ddf2418283a2f
stampil <developpeurntic@gmail.com>
2014-09-20 20:16:08 +0200

    caract

--- js/class_BattleBarFighter.js
+++ js/class_BattleBarFighter.js
@@ -1,19 +1,42 @@
 function BattleBarFighter(codebar) {
     this.codebar;
-    var privateVariable = 777;
+    this.PV;
+    this.FO;
+    var self = this;
 
     function __Construct(param){
         console.log('construct');
         this.codebar=param;
+        generate_PV();
+    }
+
+    function generate_PV(){
+        var string = this.codebar.text.toString();
+        this.PV=0;
+        for (var i=0; i<string.length; i++){
+            if(i%2==0){
+                this.PV+= parseInt(string[i])*15;
+            }
+        }
+    }
+
+    function generate_FO(){
+        var string = this.codebar.text.toString();
+        this.FO=0;
+        for (var i=0; i<string.length; i++){
+            if(i%3==0){
+                this.PV+= parseInt(string[i])*22;
+            }
+        }
     }
 
     this.save = function(){
-        ajax('setCodeBar.php','result='+codebar.text+'&format='+codebar.format+'&id_gsm=123','cb_set_code_bar');
-    }
+        ajax('setCodeBar.php','result='+self.codebar.text+'&format='+self.codebar.format+'&id_gsm=123','cb_set_code_bar');
+    };
 
     this.displayCodeBar = function(div){
-        document.getElementById(div).textContent =codebar.text;
-    }
+        document.getElementById(div).textContent =self.codebar.text+'<div id="PV"> PV:'+self.PV+'</div><div id="FO">'+self.FO+'</div>';
+    };
 
     __Construct(codebar);
 }
--- js/dom.js
+++ js/dom.js
@@ -1,7 +1,5 @@
 var el = document.querySelector("#scan_button");
-console.log(el);
 el.onclick=function(e){
-    console.log('click',e);
     scan();
 };
 
