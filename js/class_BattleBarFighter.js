3d898186286060830af95def80dba33a3c380e38
stampil <developpeurntic@gmail.com>
2014-09-20 21:06:27 +0200

    caract

--- js/class_BattleBarFighter.js
+++ js/class_BattleBarFighter.js
@@ -2,7 +2,9 @@ function BattleBarFighter(codebar) {
     this.codebar=codebar;
     this.PV;
     this.FO;
+    this.name;
     var self = this;
+    var tabName = ['an','to','ni','pa','ta','sa','me','ki','lu','bo'];
 
         generate_PV();
         generate_FO();
@@ -28,12 +30,22 @@ function BattleBarFighter(codebar) {
         }
     }
 
+    function generate_name(){
+        var string = self.codebar.text.toString();
+        self.name='';
+        for (var i=0; i<string.length; i++){
+            if(i%3==0){
+                self.name+= tabName[parseInt(string[i])%10];
+            }
+        }
+    }
+
     this.save = function(){
         ajax('setCodeBar.php','result='+this.codebar.text+'&format='+this.codebar.format+'&id_gsm=123','cb_set_code_bar');
     };
 
     this.displayCodeBar = function(div){
-        document.getElementById(div).innerHTML =this.codebar.text+'<div id="PV">PV: '+this.PV+'</div><div id="FO">FO: '+this.FO+'</div>';
+        document.getElementById(div).innerHTML =this.codebar.text+'<div id="name">'+this.name+'</div><div id="PV">PV: '+this.PV+'</div><div id="FO">FO: '+this.FO+'</div>';
     };
 
 }
