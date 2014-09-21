1e605acd11a3e33169875b5c4033547a7660b068
stampil <developpeurntic@gmail.com>
2014-09-21 11:54:37 +0200

    name generator

--- js/class_BattleBarFighter.js
+++ js/class_BattleBarFighter.js
@@ -2,15 +2,17 @@ function BattleBarFighter(codebar) {
     this.codebar=codebar;
     this.PV;
     this.FO;
+    this.ARMOR;
     this.name;
     var self = this;
-    var end = ['ith','on','ton','field','man','us','sa','oth','ion','aure','or','ob'];
+    var end = ['ith','on','ton','fer','man','us','sa','oth','ion','ius','aure','or','ob'];
     var c = ["b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v","w","x","y","z"];
     var v =["a","i","e","o","u"];
 
-        generate_PV();
-        generate_FO();
-        generate_name();
+    generate_PV();
+    generate_FO();
+    generate_ARMOR();
+    generate_name();
 
 
     function generate_PV(){
@@ -28,10 +30,22 @@ function BattleBarFighter(codebar) {
         self.FO=0;
         for (var i=0; i<string.length; i++){
             if(i%3==0){
-                self.FO+= parseInt(string[i])*22;
+                self.FO+= parseInt(string[i])*44;
             }
         }
     }
+
+    function generate_ARMOR(){
+        var string = self.codebar.text.toString();
+        self.ARMOR=0;
+        for (var i=0; i<string.length; i++){
+            if(i%3==1){
+                self.ARMOR+= parseInt(string[i])*22;
+            }
+        }
+    }
+    
+    
     function get_letter(tab,string,inc){
         return tab[(parseInt(string[inc])+ tab.length*13)% tab.length];
     }
@@ -47,7 +61,6 @@ function BattleBarFighter(codebar) {
             self.name += get_letter(c,string,inc--);
             self.name += get_letter(c,string,inc--);
             self.name += get_letter(v,string,inc--);
-            self.name += get_letter(c,string,inc--);
             self.name += get_letter(end,string,inc--);
         }
         else if(parseInt(string[1])%3==1) {
@@ -72,7 +85,7 @@ function BattleBarFighter(codebar) {
     };
 
     this.displayCodeBar = function(div){
-        document.getElementById(div).innerHTML =this.codebar.text+'<div id="name">'+this.name+'</div><div id="PV">PV: '+this.PV+'</div><div id="FO">FO: '+this.FO+'</div>';
+        document.getElementById(div).innerHTML =this.codebar.text+'<div id="name">'+this.name+'</div><div id="PV">PV: '+this.PV+'</div><div id="FO">FO: '+this.FO+'</div><div id="ARMOR">ARMOR: '+this.ARMOR+'</div>';
     };
 
 }
