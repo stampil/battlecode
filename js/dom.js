64843177c1269da444f6c6f7e6b0a0e05a6728ef
Sebastien GOURMAND <sgourmand@wyplay.com>
2014-09-22 14:53:17 +0200

    tt

--- js/dom.js
+++ js/dom.js
@@ -6,27 +6,14 @@ document.getElementById("scan_button").onclick = function (e) {
 var menu_buttons = document.getElementsByClassName("menu_button");
 var all_contents = document.getElementsByClassName("content");
 
-
-for (var i = 0; i < menu_buttons.length; i++) {
-    menu_buttons[i].onclick = function (e) {
-        document.getElementById("menu").style.display="none";
-        document.getElementById("scan").style.display="block";
-        document.getElementById("content_"+e.srcElement.id).style.display="block";
-    };
+function display_contents(value){
+    for (var i = 0; i < all_contents.length; i++) {
+            all_contents[i].style.display=value;
+        }
 }
 
-document.getElementById("title_button").onclick = function (e) {
-    document.getElementById("menu").style.display="block";
-    document.getElementById("scan").style.display="none";
-    for( var i=0; i< all_contents; i++){
-         all_content[i].style.display="none";
-    }
-    
-};
-
-
-var to_check = [type_character,type_armor, type_weapon];
-for (var i=0; i< to_check.length; i++){
+function check_save(to_check){
+    for (var i=0; i< to_check.length; i++){
     var check_cookie = cookie.get('codebar_'+to_check[i]);
 
     if(check_cookie){
@@ -35,3 +22,26 @@ for (var i=0; i< to_check.length; i++){
         BBF.displayCodeBar();
     }
 }
+}
+
+
+for (var i = 0; i < menu_buttons.length; i++) {
+    menu_buttons[i].onclick = function (e) {
+        var clicked = e.srcElement.id;
+        document.getElementById("menu").style.display="none";
+        document.getElementById("scan").style.display="block";
+        document.getElementById("content_"+clicked).style.display="block";  
+
+        if(clicked=="player_1"){
+            check_save([type_character, type_armor, type_weapon]);
+        }
+    };
+}
+
+document.getElementById("title_button").onclick = function (e) {
+    document.getElementById("menu").style.display="block";
+    document.getElementById("scan").style.display="none";
+    display_contents("none");
+    
+};
+
