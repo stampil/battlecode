f28486835cf81891eba0e67354440a1954533a54
stampil <developpeurntic@gmail.com>
2014-09-29 12:31:28 +0200

    refactor

--- js/dom.js
+++ js/dom.js
@@ -3,7 +3,7 @@ document.getElementById("scan_button").onclick = function (e) {
 };
 
 var menu_buttons = document.getElementsByClassName("menu_button");
-var all_contents = document.getElementsByClassName("content");
+
 var width = document.body.clientWidth;
 var height = document.body.clientHeight;
 
@@ -13,11 +13,7 @@ for (var i = 0; i < all_buttons.length; i++) {
     all_buttons[i].style.width = (width - 20) + 'px';
 }
 
-function display_contents(value) {
-    for (var i = 0; i < all_contents.length; i++) {
-        all_contents[i].style.display = value;
-    }
-}
+
 
 function check_save(to_check) {
     for (var i = 0; i < to_check.length; i++) {
@@ -38,51 +34,47 @@ function check_save(to_check) {
 for (var i = 0; i < menu_buttons.length; i++) {
     menu_buttons[i].onclick = function (e) {
         var clicked = e.srcElement.id;
-        display_contents("none");
+        hideClass("content");
         clearTimeout(timeout_click_fight);
-        document.getElementById("menu").style.display = "none";
-        document.getElementById("scan").style.display = "block";
-        document.getElementById("content_" + clicked).style.display = "block";
+        hide("menu");
+        show("scan","content_" + clicked);
+
 
         if (clicked == "player_1") {
-            document.getElementById("cards").style.display = "block";
+            show("cards");
             check_save([type_character, type_armor, type_weapon]);
         }
     };
 }
 
 document.getElementById("title_button").onclick = function (e) {
-
-    document.getElementById("menu").style.display = "block";
-    display_contents("none");
-
+    show("menu");
+    hideClass("content");
 };
 
 document.getElementById("start_button").onclick = function () {
     if (player_1) {
-        display_contents("none");
-        document.getElementById("cards").style.display = "block";
-        document.getElementById('combat').style.display = 'block';
-        document.getElementById("card_ennemi").style.display = "block";
-        document.getElementById("card_" + type_armor).style.display = "none";
-        document.getElementById("card_" + type_weapon).style.display = "none";
+        hideClass("content");
+        show("cards","combat","card_ennemi");
+        hide("card_" + type_armor, "card_" + type_weapon );
         document.getElementById('div_img_ennemi').classList.add('picture_equipement');
         document.getElementById('div_img_player_1').classList.add('picture_equipement');
 
         do {
-           player_2 = fake_scan('IA','card_ennemi');
+            player_2 = fake_scan('IA', 'card_ennemi');
         }
         while (player_2.type != type_character);
 
         player_1.fight(fight_attack);
         player_2.fight(fight_defense);
         fight_attack_desactivate = false;
+        show("button_attack");
     }
 }
 
-document.getElementById("td_ennemi").onclick = function () {
-    document.getElementById("result_fight").innerHTML="";
-    if (fight_attack_desactivate){
+document.getElementById("button_attack").onclick = function () {
+    document.getElementById("result_fight").innerHTML = "";
+    if (fight_attack_desactivate) {
         console.warn('P2 click desactivated');
         return false;
     }
@@ -91,21 +83,21 @@ document.getElementById("td_ennemi").onclick = function () {
     if (player_1) {
         var res1 = player_1.click_fight();
         var res2 = player_2.click_fight();
-        console.log('attaque',res1,res2);
-        if(timeout_click_fight){
+        console.log('attaque', res1, res2);
+        if (timeout_click_fight) {
             clearTimeout(timeout_click_fight);
-            timeout_click_fight=null;
+            timeout_click_fight = null;
         }
-        document.getElementById('img_ennemi').classList.remove('blink');
-        var degat = res1-res2;
-        if(degat>0){
-            console.log('degat fait',degat);
-            document.getElementById("result_fight").innerHTML="degat fait :<span class='FO'>"+degat+"</span>";
+
+        var degat = res1 - res2;
+        if (degat > 0) {
+            console.log('degat fait', degat);
+            document.getElementById("result_fight").innerHTML = "degat fait :<span class='FO'>" + degat + "</span>";
             player_2.takeDammage(degat);
-            if(player_2.PV<=0) return;
+            if (player_2.PV <= 0)
+                return;
         }
-        timeout_click_fight =setTimeout(function () {
-            document.getElementById('img_player_1').classList.add('blink');
+        timeout_click_fight = setTimeout(function () {
             player_1.fight(fight_defense);
             player_2.fight(fight_attack);
             fight_defense_desactivate = false;
@@ -114,9 +106,9 @@ document.getElementById("td_ennemi").onclick = function () {
     return false;
 }
 
-document.getElementById("td_player_1").onclick = function () {
-    document.getElementById("result_fight").innerHTML="";
-    if (fight_defense_desactivate){
+document.getElementById("button_defense").onclick = function () {
+    document.getElementById("result_fight").innerHTML = "";
+    if (fight_defense_desactivate) {
         console.warn('P1 click desactivated');
         return false;
     }
@@ -125,27 +117,25 @@ document.getElementById("td_player_1").onclick = function () {
     if (player_1) {
         var res1 = player_1.click_fight();
         var res2 = player_2.click_fight();
-        console.log('defense',res1,res2);
-        if(timeout_click_fight){
+        console.log('defense', res1, res2);
+        if (timeout_click_fight) {
             clearTimeout(timeout_click_fight);
-            timeout_click_fight=null;
+            timeout_click_fight = null;
         }
-        document.getElementById('img_player_1').classList.remove('blink');
-        var degat = res2-res1;
-        if(degat>0){
-            console.log('degat subit',degat);
-            document.getElementById("result_fight").innerHTML="degat subit :<span class='FO'>"+degat+"</span>";
+        var degat = res2 - res1;
+        if (degat > 0) {
+            console.log('degat subit', degat);
+            document.getElementById("result_fight").innerHTML = "degat subit :<span class='FO'>" + degat + "</span>";
             player_1.takeDammage(degat);
-            if(player_1.PV<=0) return;
+            if (player_1.PV <= 0)
+                return;
         }
-        timeout_click_fight =setTimeout(function () {
-            document.getElementById('img_ennemi').classList.add('blink');
+        timeout_click_fight = setTimeout(function () {
             player_1.fight(fight_attack);
             player_2.fight(fight_defense);
-            fight_attack_desactivate=false;
+            fight_attack_desactivate = false;
         }, 3000);
 
     }
     return false;
 }
-
--- js/main.js
+++ js/main.js
@@ -9,11 +9,15 @@ if (typeof cordova == 'undefined') {
 
     cordova.plugins.barcodeScanner.scan = function () {
         //document.getElementById('ret').textContent='Scan non disponible';
-        fake_scan(2);
+        o = fake_scan(2);
+        if (o.type == type_character) {
+            player_1 = o;
+        }
+
     }
 }
 
-function fake_scan(format,where){
+function fake_scan(format, where) {
     var BBF = new BattleBarFighter({text: Math.round((Math.random() * 1000000000000) + 1000000000000).toString(), format: format});
     BBF.displayCodeBar(where);
     return BBF;
@@ -21,29 +25,29 @@ function fake_scan(format,where){
 
 function scan() {
     cordova.plugins.barcodeScanner.scan(
-        function (result) {
-            /*
-             result.text result.format  result.cancelled
-             */
+            function (result) {
+                /*
+                 result.text result.format  result.cancelled
+                 */
 
 
-            if (result.text) {
-                var BBF = new BattleBarFighter(result);
-                BBF.save();
-                BBF.displayCodeBar();
-                if (BBF.type == type_character) {
-                    player_1 = BBF;
+                if (result.text) {
+                    var BBF = new BattleBarFighter(result);
+                    BBF.save();
+                    BBF.displayCodeBar();
+                    if (BBF.type == type_character) {
+                        player_1 = BBF;
+                    }
                 }
-            }
-            else {
-                document.getElementById('ret').textContent = 'Scanning cancelled';
-            }
+                else {
+                    document.getElementById('ret').textContent = 'Scanning cancelled';
+                }
 
-        },
-        function (error) {
-            var ret = "Scanning failed: " + error;
-            document.getElementById('ret').textContent = ret;
-        }
+            },
+            function (error) {
+                var ret = "Scanning failed: " + error;
+                document.getElementById('ret').textContent = ret;
+            }
     );
 }
 
@@ -54,4 +58,45 @@ function ajax(page, param, callback) {
     script.setAttribute('type', 'text/javascript');
     script.setAttribute('src', 'http://vps36292.ovh.net/mordu/BATTLECODE/' + page + '?' + param + '&callback=' + callback);
     head.appendChild(script);
+}
+
+function display(display, tab) {
+    for (var i = 0; i < tab.length; i++) {
+        elId = tab[i];
+        if (document.getElementById(elId)) {
+            document.getElementById(elId).style.display = display;
+            console.info(display, '#'+elId);
+        }
+        else {
+            console.error(elId, 'not an dom id to ' + display);
+        }
+    }
+    ;
+}
+
+function displayClass(display, tab) {
+    for (var i = 0; i < tab.length; i++) {
+        elClass = tab[i];
+        var els = document.getElementsByClassName(elClass);
+        for (var j = 0; j < els.length; j++) {
+            els[j].style.display = display;
+            console.info(display,'.'+els[j].className);
+        }
+    }
+}
+
+function show() {
+    display("block", arguments);
+}
+
+function hide() {
+    display("none", arguments);
+}
+
+function showClass() {
+    displayClass("block", arguments);
+}
+
+function hideClass() {
+    displayClass("none", arguments);
 }
