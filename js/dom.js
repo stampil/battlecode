c6d2a9524449e202f17f4c4e281e91e429e58bd0
stampil <developpeurntic@gmail.com>
2014-09-30 16:43:26 +0200

    t

--- js/dom.js
+++ js/dom.js
@@ -146,3 +146,11 @@ document.getElementById("button_defense").onmousedown = function () {
     }
     return false;
 }
+function test(){
+cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
+            alert("encode success: " + success);
+          }, function(fail) {
+            alert("encoding failed: " + fail);
+          }
+        );
+}
